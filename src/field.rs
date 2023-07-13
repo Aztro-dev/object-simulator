9b6f0923256ef96739de426941b1c1df95832fb6
Aztro-dev <davidperezcastellanos@gmail.com>
2023-07-12 23:26:16 -0500

    I mean it compiles, but it doesn't spawn in the thingy

--- src/field.rs
+++ src/field.rs
@@ -1,23 +1,51 @@
+use crate::ToggleVisibility;
+use crate::ToggleVisibilityRes;
+use bevy::gltf::Gltf;
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
 
-// TODO: Actually spawn in the spin up field
-pub fn setup_field(
+pub struct FieldPlugin;
+
+impl Plugin for FieldPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(setup_field.before(spawn_field));
+    }
+}
+
+#[derive(Resource)]
+pub struct FieldAssetPack(Handle<Gltf>);
+
+pub fn setup_field(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let gltf = asset_server.load("JustTheField-V2.glb");
+    commands.insert_resource(FieldAssetPack(gltf));
+}
+
+pub fn spawn_field(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+    my: Res<FieldAssetPack>,
+    assets_gltf: Res<Assets<Gltf>>,
+    toggle_visibility: Res<ToggleVisibilityRes>,
 ) {
-    // Spawn the platform
-    let size = 16.0;
-    commands
-        .spawn(PbrBundle {
-            mesh: meshes.add(shape::Box::new(2.0 * size, 0.125 * size, 2.0 * size).into()),
-            transform: Transform::from_xyz(0.0, -size, 0.0),
-            material: materials.add(Color::hex("000000").unwrap().into()),
-            ..default()
-        })
-        .insert((
-            RigidBody::Fixed,
-            Collider::cuboid(size, 0.0625 * size, size),
-            ToggleVisibility {},
-        ));
+    let object_debug_color = Color::hex("#333333").unwrap();
+    let object_size = 4.0;
+    if let Some(gltf) = assets_gltf.get(&my.0) {
+        commands
+            .spawn((
+                RigidBody::Fixed,
+                TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)),
+                Collider::cuboid(object_size, 0.5, object_size),
+                ColliderDebugColor(object_debug_color.into()),
+                ToggleVisibility {},
+            ))
+            .insert(SceneBundle {
+                scene: gltf.scenes[0].clone(),
+                visibility: if toggle_visibility.0 {
+                    Visibility::Visible
+                } else {
+                    Visibility::Hidden
+                },
+                transform: Transform::from_scale(Vec3::splat(3.0 / 7.0)),
+                ..default()
+            });
+    }
 }
