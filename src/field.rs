bcdc16eff3a15b4e26ffc948c6c394f126091559
Aztro-dev <davidperezcastellanos@gmail.com>
2023-07-16 20:37:53 -0500

    Used a function to create the wall hitboxes, tried a macro at first tho

--- src/field.rs
+++ src/field.rs
@@ -23,13 +23,12 @@ pub fn spawn_field(
 ) {
     let field = asset_server.load("JustTheField-V2.glb#Scene0");
 
-    let field_debug_color = Color::hex("#000000").unwrap();
     commands
         .spawn((
             RigidBody::Fixed,
             TransformBundle::from(Transform::from_xyz(0.0, 0.0, 0.0)),
             Collider::cuboid(FIELD_SIZE, FIELD_THICKNESS, FIELD_SIZE),
-            ColliderDebugColor(field_debug_color.into()),
+            ColliderDebugColor(FIELD_DEBUG_COLOR.into()),
             ToggleVisibility {},
         ))
         .insert(SceneBundle {
@@ -48,46 +47,37 @@ pub fn spawn_field(
         });
 }
 
+fn spawn_wall_hitbox(
+    x: f32,
+    z: f32,
+    flip: bool,
+) -> (RigidBody, TransformBundle, Collider, ColliderDebugColor) {
+    return (
+        RigidBody::Fixed,
+        TransformBundle::from(Transform {
+            translation: Vec3::new(
+                x * FIELD_SIZE / 2.35,
+                FIELD_HEIGHT + WALL_SIZE,
+                z * FIELD_SIZE / 2.35,
+            ),
+            rotation: if flip {
+                Quat::from_rotation_y(PI / 2.0)
+            } else {
+                Quat::default()
+            },
+            ..default()
+        }),
+        Collider::cuboid(0.0, WALL_SIZE, FIELD_SIZE / 2.3),
+        ColliderDebugColor(FIELD_DEBUG_COLOR.into()),
+    );
+}
+
 const WALL_SIZE: f32 = 6.0;
+const FIELD_DEBUG_COLOR: Color = Color::rgb(0.0, 0.0, 0.0);
 pub fn spawn_hitboxes(mut commands: Commands) {
-    let field_debug_color = Color::hex("#000000").unwrap();
     // Walls
-    commands.spawn((
-        RigidBody::Fixed,
-        TransformBundle::from(Transform {
-            translation: Vec3::new(-FIELD_SIZE / 2.35, FIELD_HEIGHT + WALL_SIZE, 0.0),
-            ..default()
-        }),
-        Collider::cuboid(0.0, WALL_SIZE, FIELD_SIZE / 2.3),
-        ColliderDebugColor(field_debug_color.into()),
-    ));
-    commands.spawn((
-        RigidBody::Fixed,
-        TransformBundle::from(Transform {
-            translation: Vec3::new(FIELD_SIZE / 2.35, FIELD_HEIGHT + WALL_SIZE, 0.0),
-            ..default()
-        }),
-        Collider::cuboid(0.0, WALL_SIZE, FIELD_SIZE / 2.3),
-        ColliderDebugColor(field_debug_color.into()),
-    ));
-    commands.spawn((
-        RigidBody::Fixed,
-        TransformBundle::from(Transform {
-            translation: Vec3::new(0.0, FIELD_HEIGHT + WALL_SIZE, FIELD_SIZE / 2.35),
-            rotation: Quat::from_rotation_y(PI / 2.0),
-            ..default()
-        }),
-        Collider::cuboid(0.0, WALL_SIZE, FIELD_SIZE / 2.3),
-        ColliderDebugColor(field_debug_color.into()),
-    ));
-    commands.spawn((
-        RigidBody::Fixed,
-        TransformBundle::from(Transform {
-            translation: Vec3::new(0.0, FIELD_HEIGHT + WALL_SIZE, -FIELD_SIZE / 2.35),
-            rotation: Quat::from_rotation_y(PI / 2.0),
-            ..default()
-        }),
-        Collider::cuboid(0.0, WALL_SIZE, FIELD_SIZE / 2.3),
-        ColliderDebugColor(field_debug_color.into()),
-    ));
+    commands.spawn(spawn_wall_hitbox(1.0, 0.0, false));
+    commands.spawn(spawn_wall_hitbox(-1.0, 0.0, false));
+    commands.spawn(spawn_wall_hitbox(0.0, 1.0, true));
+    commands.spawn(spawn_wall_hitbox(0.0, -1.0, true));
 }
